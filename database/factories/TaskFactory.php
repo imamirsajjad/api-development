2c0937d2d9e212ed2f85f7c7d4753362db906daf
imamirsajjad <imamirsajjad@gmail.com>
2025-06-30 00:36:11 +0500

    created TaskControlller, TaskModel, TaskFactory  and seed them

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
+ */
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => \App\Models\User::factory(),
+            'name' => $this->faker->name(),
+            'status' => $this->faker->randomElement([0, 1, 2]),
+        ];
+    }
+}
